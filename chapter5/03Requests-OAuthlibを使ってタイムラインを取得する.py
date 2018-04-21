cbc17890cfebe066961b32fae006d5d9164cf47d
kyohei koga <koga@nsri.nssmc.com>
2018-04-21 23:03:37 +0900

    text ignore

---
+++ chapter5/03Requests-OAuthlibを使ってタイムラインを取得する.py
@@ -0,0 +1,6 @@
+import os
+import lxml
+import pymongo
+
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+print(CONSUMER_KEY)
---
+++ chapter5/excute.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+
+echo "hello world"
+
+. deactivate py36con
+. activate py36con
+
+python ~/crawling-scraping/chapter5/03Requests-OAuthlibを使ってタイムラインを取得する.py
